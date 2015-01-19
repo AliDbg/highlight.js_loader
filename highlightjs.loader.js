e1a6dda6219d994950d21bec8d3a3c1d4a9050b6
Ali.Dbg <ali.debugger@gmail.com>
2015-01-19 20:09:10 +0330

    Version 3.0
    Released v3.0

--- highlightjs.loader.js
+++ highlightjs.loader.js
@@ -1,30 +1,27 @@
-/*! Highlight JS Loader v2.0 | https://github.com/alidbg/highlight.js_loader */
-(function() {
-    'use strict';
-    var doc = document;
-    doc.write("\n<style>code{border-radius:.4em;word-wrap:normal;direction:ltr;white-space:pre;overflow:auto;max-height:50em;margin:.5em auto;padding:.5em;background-color:#eee;display:block}[class^=\"hljs-\"],[class*=\" hljs-\"]{margin:0;padding:0;}code .linum{display:block;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;cursor:default;text-align:right;float:left;padding:0;margin:.1em 1em .1em -1em;border-right:.1em solid}code .linum span{padding:0 .5em 0 1em;display:block}<\/style>");
+/*! Highlight JS Loader v3.0 | https://github.com/alidbg/highlight.js_loader */
+(function(doc) {
+    "use strict";
+    doc.write("\n<style>.hljs,.hljs span{font-family:Consolas,Monaco,monospace;line-height:1.45;word-wrap:normal;position:relative;float:none;direction:ltr}.hljs{border-radius:.4em;max-height:40em;margin:.5em auto;white-space:pre;overflow:auto}.hljs .hjln{cursor:default;text-align:right;float:left;margin:0 1em .1em -1em;border-right:.1em solid;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none}.hljs .hjln span{padding:0 .5em 0 1em;display:block}</style>");
     doc.addEventListener("DOMContentLoaded", function() {
-        var code = doc.getElementsByTagName("code"), u = 'undefined', i = 0;
-        for (i; i < code.length; i++) {
-            var c = code[i], o = "onclick", s = "selhj(this)";
-            if(c.getAttribute(o) == s) break;
+        var code = doc.getElementsByTagName("code");
+        for (var i = 0; i < code.length; i++) {
+            var c = code[i];
+            c.addEventListener("click", function() {
+                if (confirm("Select All?")) {
+                    var r = doc.createRange(),
+                        s = window.getSelection();
+                    s.removeAllRanges();
+                    r.setStart(this, 1);
+                    r.setEnd(this, this.childNodes.length);
+                    s.addRange(r)
+                }
+            });
             c.innerHTML = c.innerHTML.replace(/<br[^>]*>$/mgi, "").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
-            c.setAttribute(o, s);
-            if (typeof hljs != u) hljs.highlightBlock(c);
-            c.innerHTML = '<span class="linum"></span>' + c.innerHTML;
-            var g = c.innerHTML.split(/\n/).length, a = 1;
-            for (a; a <= g; a++)(c.getElementsByTagName("span")[0]).innerHTML += "<span>" + a + "</span>"
+            hljs.highlightBlock(c);
+            c.innerHTML = '<span class="hjln"></span>' + c.innerHTML;
+            var g = c.innerHTML.split(/\n/).length;
+            for (var a = 1; a <= g; a++)(c.getElementsByTagName("span")[0]).innerHTML += "<span>" + a + "</span>"
         }
-        if (typeof jQuery != u && jQuery.fn.niceScroll) $("code").niceScroll()
+        if (typeof jQuery != "undefined" && jQuery.fn.niceScroll) $("code").niceScroll()
     })
-})();
-
-var selhj = function(i) {
-    if (!confirm("Select All?")) return 0;
-    var r = document.createRange(),
-        s = window.getSelection();
-    s.removeAllRanges();
-    r.setStart(i, 1);
-    r.setEnd(i, i.childNodes.length);
-    s.addRange(r)
-};
+})(document);
